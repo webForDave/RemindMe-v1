8616679d98711f1b5cfa80dd4737e28d2d0dbab3
David Akinola <dakinola54@gmail.com>
2025-03-23 16:26:20 +0100

    beginning steps for reminders

--- accounts/models.py
+++ accounts/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
--- reminders/models.py
+++ reminders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Reminder(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateField(default=None, blank=True)
+    contact_details = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
