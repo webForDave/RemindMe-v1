a1647f9432e7a3a220129f3927722366cd47aabb
David Akinola <dakinola54@gmail.com>
2025-03-23 15:40:42 +0100

    registered custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["username", "email", "name", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = ((None, {
+        "classes": ("wide",),
+        "fields": ("username", "email", "name", "password1", "password2",),
+    }),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
-class CustomUserChangeForm(UserChangeForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
+        fields = UserCreationForm.Meta.fields + ("name",)
         model = CustomUser
-        fields = UserChangeForm.Meta.fields + ("name",)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
         fields = UserChangeForm.Meta.fields
+        model = CustomUser
