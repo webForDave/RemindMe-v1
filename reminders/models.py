09dfd5d41ee3f294b0b52e2d43f77e398d9d6fc5
David Akinola <dakinola54@gmail.com>
2025-05-27 23:08:42 +0100

    initial commit: config & reminder model

---
+++ reminders/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Reminder(models.Model):
+
+    NOTIFICATION_CHOICES = [
+        ('email', 'Email'),
+        ('sms', 'SMS'),
+        ('both', "Email & SMS")
+    ]
+
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    reminder_time = models.DateTimeField()
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_CHOICES, default="email")
+    contact = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
