f0f8f898f507a8fbe666a2284a19c5eefd034145
David Akinola <dakinola54@gmail.com>
2025-05-27 22:25:44 +0100

    starting over

--- accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.email
